921822227983d6524fb495cbaa1474521d2e3d16
GulTion <gkv8192@gmail.com>
2020-07-02 07:09:58 -0700

    fd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
 app.get('/', (req,res)=>{
-	res.send(`<h1>You are in ${req.query.name}</h1>`)
+	res.send(`<h1>You are in  ${req.query.name}</h1>`)
 })
 app.listen(process.env.PORT||3000)
