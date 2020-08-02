ad06feb553c2e9663618e4fee2336e49777343bb
GulTion <gkv8192@gmail.com>
2020-08-02 16:42:34 +0530

    Api is working 60%

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const express = require('express')
+const express = require('express');
+
 const app = express();
+var request = require("request");
+
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
 app.get('/', (req,res)=>{
 	res.send(`<h1>You are in  ${req.query.name} Change from vscode</h1>`)
 })
-app.listen(process.env.PORT||3000)
+app.get('/csalgo',(req,res)=>{
+	var jar = request.jar();
+jar.setCookie(request.cookie("__cfduid=d14e8dbb1ba079fa5920f0a00014fdb671596352599"), req.query.url);
+
+var options = {
+  method: 'POST',
+  url: req.query.url,
+  headers: {
+    'content-type': 'application/json',
+    authorization: 'Basic SGFySGFyTWFoYWRldjpIYXJIYXJNYWhhZGV2'
+  },
+  body: {q: '', password: null, page_token: null, page_index: 0},
+  json: true,
+  jar: 'JAR'
+};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  res.json(body)
+});
+
+})
+app.listen(process.env.PORT||3001,()=>{                                                            
+	console.log('Server is Starting');
+})
