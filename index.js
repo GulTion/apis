96486333133e43e8881295f051d51f090f8138bb
GulTion <gkv8192@gmail.com>
2020-08-03 17:06:31 +0530

    HeroKu

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const libgen = require('libgen')
 const app = express();
 var request = require("request");
-
-
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -13,10 +13,15 @@ app.use(function(req, res, next) {
 app.get('/', (req,res)=>{
 	res.send(`<h1>You are in  ${req.query.name} Change from vscode</h1>`)
 })
-app.get('/csalgo',(req,res)=>{
-	var jar = request.jar();
-jar.setCookie(request.cookie("__cfduid=d14e8dbb1ba079fa5920f0a00014fdb671596352599"), req.query.url);
+app.post('/libgen',async (req,res)=>{
 
+  console.log("Searching for "+req.body.query)
+  const books =await libgen.search(req.body)
+  console.log('Complete')
+  res.json(books)
+})
+app.get('/csalgo',(req,res)=>{
+console.log(`calling for ${req.query.url}`)
 var options = {
   method: 'POST',
   url: req.query.url,
@@ -29,9 +34,10 @@ var options = {
   jar: 'JAR'
 };
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
 
+request(options, function (error, response, body) {
+
+  if (error) throw new Error(error);
   res.json(body)
 });
 
