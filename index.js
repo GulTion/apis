f6f7cd90f131723073a7064d92ac606d0b91929a
GulTion <Gkv8192@gmail.com>
2020-12-20 12:07:48 +0000

    ByteBin api implemented

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const libgen = require('libgen')
+const libgen = require('libgen');
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+const fileUpload = require('express-fileupload');
+const body_paraser = require('body-parser');
 const app = express();
 
-
-// DataBase
 const JsonDB  = require('node-json-db');
 const Config = require('node-json-db/dist/lib/JsonDBConfig')
+
+const db = new JsonDB.JsonDB(new Config.Config("myDataBase", true, false, '/'));
+
+app.use(fileUpload());
+
+// DataBase
+
  
-const db = new JsonDB.JsonDB(new Config.Config("AEC", true, false, '/'))
-const libdb = new JsonDB.JsonDB(new Config.Config("LIB", true, false, '/'))
 // 
 const axios = require('axios')
 var request = require("request");
@@ -20,11 +28,6 @@ app.use(function(req, res, next) {
 	next();
   });
 
-app.get('/', (req,res)=>{
-  db.push('/black/first[]',[Math.random(),Math.random(),Math.random()],true);
-  console.log(db.getData('/black/first'));
-	res.send(`<h1>Welcome to GulTion API</h1>`)
-})
 function urlDown(md5){
   try{
   const c = libgen.utils.check.canDownload(md5);
@@ -118,6 +121,244 @@ app.get('/cse',(r,s)=>{
 console.log(db.getData('/test'));
   s.send('CSE')
 })
+
+const token = fs.readFileSync('token.json')
+const auth = JSON.parse(token);
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+
+const TOKEN_PATH = 'token.json';
+
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+const RandomKey = (a,b)=>{
+  return Number(Math.floor(a + (b-a)*Math.random())).toString(10);
+}
+
+app.post('/bytebin',(req,res)=>{
+
+    console.log(req.files);
+    
+    let _file = req.files.file;
+    _file.mv('./upload/' + _file.name)
+
+
+
+
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  authorize(JSON.parse(content), uploadfiletodrive);
+});
+
+
+const uploadfiletodrive=(auth)=>{
+  const drive = google.drive({version: 'v3', auth});
+var fileMetadata = {
+  'name': _file.name,
+  'mimeType': _file.mimetype,
+  'parents':['1kXbR8I8QgS4JxzzQxnJnVvpaRy_IATCq']
+};
+var media = {
+  mimeType: _file.mimetype,
+  body: fs.createReadStream("./upload/"+_file.name)
+};
+drive.files.create({
+  resource: fileMetadata,
+  media: media,
+  fields: 'id'
+}, function (err, file) {
+  if (err) {
+    // Handle error
+    console.error(err);
+  } else {
+    console.log('File Id:', file.data.id);
+    const MetaData = {
+      name:_file.name,
+      size:_file.size,
+      mimeType:_file.mimetype,
+      gid:file.data.id
+    };
+    if(req.query.mode==2){
+        const {id,key}=req.query;
+        const got=db.getIndex('/mainArr',Number(id));
+        if(got!=-1){
+        const _block = db.getData(`/mainArr[${got}]`);
+        if(_block.key==key){
+          db.push(`/mainArr[${got}]/files[]`,MetaData,true);
+          res.send({files:db.getData(`/mainArr[${got}]`).files,mode:2})
+        }else{
+
+        }
+        }
+    }
+
+    if(req.query.mode=="0"){
+    const now = db.getData('/now');
+    const key = RandomKey(1111,9999);
+
+    db.push('/mainArr[]',{id:now,key:key, files:[MetaData]},true);
+    res.send({id:now, key:key});
+    db.push('/now',now+1); 
+    }
+    fs.unlink("./upload/"+_file.name, (err) => {
+    if (err) throw err;
+    console.log(_file.name+" is Deleted");
+    
+});
+  }
+});
+}
+
+
+})
+
+app.get('/bytebin/:id',(req,res)=>{
+  console.log(req.params);
+  res.send("Public");
+})
+
+app.post('/getbytebinforadmin',(req,res)=>{
+  const {key, id} = req.body;
+  console.log(req.body);
+  const got = db.getIndex('/mainArr',Number(id));
+  if(got!=-1){
+  const _current = db.getData(`/mainArr[${got}]`);
+  if(_current.key==key){
+    res.send({files:_current.files,mode:2});
+  }else{
+    res.send({files:_current.files,mode:1});
+  }
+  }else{
+    res.send({files:[],mode:0})
+  }
+})
+
+app.get('/driveinit',(req,res)=>{
+  // If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Drive API.
+  authorize(JSON.parse(content), listFiles);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listFiles(auth) {
+  const drive = google.drive({version: 'v3', auth});
+  drive.files.list({
+    pageSize: 10,
+    fields: 'nextPageToken, files(id, name)',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const files = res.data.files;
+    if (files.length) {
+      console.log('Files:');
+      files.map((file) => {
+        console.log(`${file.name} (${file.id})`);
+      });
+    } else {
+      console.log('No files found.');
+    }
+  });
+}
+res.send("DONE");
+})
+
+
 app.listen(process.env.PORT||3001,()=>{                                                            
 	console.log('Server is Starting');
 })
